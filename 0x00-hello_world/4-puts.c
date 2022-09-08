5f0a3d9edcc3bf557ebde7e81f1d45ca2257c56b
Adaobitech <adaobiezenwelu@gmail.com>
2022-09-08 10:34:20 -0700

    Task 4 Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/* main - Entry point
+ *
+ * function - puts
+ *
+ * Return - Always 0
+ *
+ * puts - Standard output
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle \n")
+		return (0)
+}
