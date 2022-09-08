9ba4c86d9538b0da34e58c00ca5fc05cdc76c442
Adaobitech <adaobiezenwelu@gmail.com>
2022-09-08 13:33:18 -0700

    Task 6 Sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,8 +3,8 @@
  * main - Entry point
  *
  * char, int, float - Datatypes
- * 
- * Return: Always 0
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
@@ -20,5 +20,5 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long) sizeof(c));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-	return(0);
+	return (0);
 }
