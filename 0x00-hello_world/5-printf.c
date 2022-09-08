bae5cbcc849ca7d8b6b856070e0d05765b82273f
Adaobitech <adaobiezenwelu@gmail.com>
2022-09-08 11:28:43 -0700

    Task 5 print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+/**
+ * main - Entry point
+ *
+ * printf - standard output
+ *
+ * Return: 0 (Success)
+ */
+#include <stdio.h>
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+		return(0);
+}
