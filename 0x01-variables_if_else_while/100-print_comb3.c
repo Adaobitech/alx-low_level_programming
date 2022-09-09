54da37d761baad2db09d576664b73ce5f2ecd53a
Adaobitech <adaobiezenwelu@gmail.com>
2022-09-09 16:41:19 -0700

    Task 10 two digits combo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,11 +13,11 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n < 100; n++)
+	for (n = 0; n < 90; n++)
 	{
 		putchar((n / 10) + '0');
 		putchar((n % 10) + '0');
-		if (n != 99)
+		if (n != 89)
 		{
 			putchar(',');
 			putchar(' ');
