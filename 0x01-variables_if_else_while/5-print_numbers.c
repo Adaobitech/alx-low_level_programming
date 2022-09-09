5ac78bd3ef17837334fbf535c1a14111e09b7f78
Adaobitech <adaobiezenwelu@gmail.com>
2022-09-09 14:27:55 -0700

    Task 5 Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib>
+#include <time>
+/**
+ * main - Entry point
+ *
+ * code that prints decimal numbers
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+
+	return (0);
+}
