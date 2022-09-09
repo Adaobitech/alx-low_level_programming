0753ffd80390f9b94b8e8abf4f4c8b8889df2456
Adaobitech <adaobiezenwelu@gmail.com>
2022-09-09 15:09:55 -0700

    Task 7 Reverse alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Code that prints alphabets in reverse form
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	       putchar(ch);
+	
+	putchar('\n');
+	
+	return (0);
+}	
