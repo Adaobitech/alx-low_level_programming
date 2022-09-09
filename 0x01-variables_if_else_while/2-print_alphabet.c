fe3787e02abe5a71b99831fb28e4fef10fb2b5df
Adaobitech <adaobiezenwelu@gmail.com>
2022-09-09 12:39:18 -0700

    Task 2 Alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Code prints the alphabets in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+		putchar('\n');
+
+	return (0);
+}
