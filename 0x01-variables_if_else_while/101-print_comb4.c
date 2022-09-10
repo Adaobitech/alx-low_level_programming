a704f8aa6e4c71c7cad4624f48431fa026024f96
Adaobitech <adaobiezenwelu@gmail.com>
2022-09-10 04:53:57 -0700

    Task 11 three digit combo

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Code that prints smallest combination of three digits
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 48; a < 58; a++)
+	{
+		for (b = 49; b < 58; b++)
+		{
+			for (c = 50; c < 58; c++)
+			{
+				if (a > b && b > c)
+				{
+					putchar(c);
+					putchar(b);
+					putchar(a);
+					if (c != 55 || b != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
