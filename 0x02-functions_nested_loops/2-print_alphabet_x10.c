bb9c93a2207027cb7f1be1a40b89efeef0a6e749
Adaobitech <adaobiezenwelu@gmail.com>
2022-09-13 08:27:16 -0700

    Task 2 Alphabets x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Checks the code
+ *
+ * Code that prints alphabets 10times
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+	
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		for ( i = 0; i <= 10; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n')
+	}
+}
