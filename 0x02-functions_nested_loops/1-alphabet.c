4f44c595592d8ac1adb0270e7dc4f043828fabd0
Adaobitech <adaobiezenwelu@gmail.com>
2022-09-13 07:50:55 -0700

    Task 1 Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Code prints alohabet in lowercase
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void);
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+
+	_putchar('\n');
+
+	return;
+}
