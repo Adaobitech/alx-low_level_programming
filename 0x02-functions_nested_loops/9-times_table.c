849524d25b3a6b0b4be0cf6d8f35e08dfaad35b3
Adaobitech <adaobiezenwelu@gmail.com>
2022-09-13 21:50:37 -0700

    Task 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,35 +11,33 @@ void times_table(void)
 {
 	int row, column, product, x, y;
 	
+	for (row = 0; row <= 9; row++);
 	{
-		for (row = 0; row <= 9; row++);
+		for (column = 0; column <= 9; column++);
 		{
-			for (column = 0; column <= 9; column++);
-			{
-				product = row * column;
-				x = product / 10;
-				y = product % 10;
+			product = row * column;
+			x = product / 10;
+			y = product % 10;
 
-				if (column == 0)
-				{
-					_putchar('0');
-				}
-				else if (product < 10)
-				{	
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(y + '0');
-				}
-				else
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(x + '0');
-					_putchar(y + '0');
-				}
+			if (column == 0)
+			{
+				_putchar('0');
 			}
-		_putchar('\n');
+			else if (product < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(y + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(x + '0');
+				_putchar(y + '0');
+			}
 		}
+		_putchar('\n');
 	}
 }
