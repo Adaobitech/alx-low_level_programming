6b67e3711d60a9e062b50935a3cfe4edb66b189a
Adaobitech <adaobiezenwelu@gmail.com>
2022-09-13 21:55:32 -0700

    Task 10 add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * add - Entry point
+ *
+ * Codes that adds two integers
+ *
+ * Return: Integers
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
