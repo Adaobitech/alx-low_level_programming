d2dd3052a4335c15a017b9346099c85fe8f0a095
Adaobitech <adaobiezenwelu@gmail.com>
2022-09-15 13:38:38 -0700

    Task 2 Multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ * code that multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: Integer
+ */
+
+int mul(int a, int b)
+{
+	int mult;
+
+	mult = a * b;
+
+	return (mult);
+}
