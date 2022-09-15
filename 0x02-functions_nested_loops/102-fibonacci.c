c6236ccb40edae95b4728e8f41384356cde1c84a
Adaobitech <adaobiezenwelu@gmail.com>
2022-09-15 02:49:33 -0700

    Task 14 Fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Code to print the first 50 fibonacci numbers
+ *
+ * Retrn: Always 0 (success)
+ */
+
+int main(void)
+{
+	long int i, j, k, n;
+
+	i = 1;
+	j = 2;
+	n = 2;
+
+	printf("%lu, ", i);
+	while (n <= 50)
+	{
+		if (n == 50)
+		{
+			printf("%lu\n", j);
+		}
+		else
+		{
+			printf("%lu, ", j);
+		}
+
+		k = j;
+		j += i;
+		i = k;
+
+		n++;
+	}
+	return (0);
+}
