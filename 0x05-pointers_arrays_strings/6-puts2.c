591d98073f559fa021a05a23bf73cafea55f289b
Adaobitech <adaobiezenwelu@gmail.com>
2022-09-19 15:22:27 -0700

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every character
+ *
+ * @str: holds string
+ *
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int a;
+
+	a = o;
+	while (str[a] != '\0')
+	{
+		if (a % 2 == 0)
+			_putchar(str[a];
+			a++;
+	}
+	_putchar('\n');
