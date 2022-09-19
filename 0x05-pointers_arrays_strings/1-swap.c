fb416927efb330832a19a1bf87f2521dcb9f7c65
Adaobitech <adaobiezenwelu@gmail.com>
2022-09-19 14:01:06 -0700

    Task 1 Swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - Swaps two integers
+ * @a: a is forst integer
+ * @b: b is second integer
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
