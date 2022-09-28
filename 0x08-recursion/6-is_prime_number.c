0e489e23db96ebcb5524d2bf44601f17396bdcef
Adaobitech <adaobiezenwelu@gmail.com>
2022-09-28 16:00:18 -0700

    Task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _is_prime _number - prints prime number
+ * @n: Integer
+ * Return: 1 if Prime numbea,0 if not
+ */
+
+int _is_prime_number(int n)
+{
+	int start = n / 2;
+
+	if (n <= 1)
+		return (0);
+	return (is_prime(n, start));
+}
+
+/**
+ * is_prime - return 1 if n isgreh
+ * @: String
+ * @start: String
+ * Return: prime number
+ */
+
+int is_prime(int n, int start)
+{
+	if (n <= 1)
+		return (1);
+	else if (n % start == 0)
+		return (0);
+	return (is_prime(n, start - 1));
+}
