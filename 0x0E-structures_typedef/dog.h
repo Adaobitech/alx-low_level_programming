06547fec28fecf7837ab041b98ee5be38c05083c
Adaobitech <adaobiezenwelu.gmail.com>
2022-10-10 21:41:36 -0700

    Task 0 dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - description of a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: This describes the properties of a dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
