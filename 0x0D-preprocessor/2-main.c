8400e54479cdb957df1d76eebb1e389fcca242ca
Adaobitech <adaobiezenwelu.gmail.com>
2022-10-13 05:09:01 -0700

    Task 2 FILE

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,17 @@
+#ifndef __FILE__
+#define __FILE__
+#endif
+#include <stdio.h>
+
+
+/**
+ * main - prints the name of the file
+ *
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
