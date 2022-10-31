c7b22bec5fae3cdfaae4cb46c5194daa23315e88
Adaobitech <adaobiezenwelu@gmail.com>
2022-10-31 16:09:28 -0700

    Header File

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to
+ * POSIX Stdout
+ * @filename: Name of file
+ * @letters: Number of letters to be read and printed
+ *
+ * Return: Actual number of letters read
+ * Return 0 if file cannot be read or opened
+ * if filename is NULL, return 0
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int f;
+	ssize_t nrd, nwr;
+	char *buf;
+
+	if (!filename)
+		return (0);
+
+	f = open(filename, O_RDONLY);
+
+	if (f == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	nrd = read(f, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
+	clode(f);
+
+	free(buf);
+
+	return (nwr);
+}
